1a93b1d0ac9da23529ea9bf69a0c1d6a63a3d48f
Nnadi Victory <chikamagozi25@gmail.com>
2024-01-27 21:08:14 +0100

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,8 +3,8 @@ import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async () => {
     async function getCoins() {
-        const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=cosmos-ecosystem&order=market_cap_desc&per_page=100&page=1&sparkline=false&x_cg_api_key=CG-CM71sa8LaFkGwEXibcBXZzqB')
-        console.log(JSON.stringify(res, null, 2))
+        const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=cosmos-ecosystem&order=market_cap_desc&per_page=100&page=1&sparkline=false&x_cg_demo_api_key=CG-CM71sa8LaFkGwEXibcBXZzqB')
+        console.log(res.status, res.statusText)
         const data = await res.json()
         
         return data;
