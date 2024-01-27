7ff4c09af45ee927654a301097ad736657aaa159
Nnadi Victory <chikamagozi25@gmail.com>
2024-01-27 21:11:52 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,12 @@ const config = {
 	preprocess: [vitePreprocess({})],
 
 	kit: {
-		adapter: adapter({})
+		adapter: adapter({
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
 	}
 };
 
